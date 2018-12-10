15bccaaf0b77006031a59c2d7e53626a2b3dd013
csPython <cspython@AD25TF04NGF1J-42.local>
2018-12-10 10:28:42 -0500

    first commit

---
+++ questionNC.py
@@ -0,0 +1,10 @@
+#questionsNC.py
+#init. vars. 
+
+q1 = bool(false)
+q1ans = int(0)
+q1ask = str("1. What is the most popular coding language?"
+            "a. Elixir"
+            "b. Python"
+            "c. JavaScript"
+            "d. Swift.")
